d1bcfe385e15aa91cb62ebdd9d223b7431b0b9ca
Ephraimiyanda <iyandaephraim@gmail.com>
2024-01-12 20:44:18 +0100

    added new script for nextjs to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import { useRouter } from "next/navigation";
 import NextUiProvider from "./Providers/NEXTUIProvider/Nextuiprovider";
 import "./globals.css";
 import Nav from "./components/Nav";
-import Script from "next/script";
-import React from "react";
+import { ReactNode } from "react";
 import { LayoutProps } from "@/.next/types/app/layout";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -16,12 +15,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children:ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Script src="https://third-party-script.js" async />
         <NextUiProvider>
           <Nav />
           {children}
