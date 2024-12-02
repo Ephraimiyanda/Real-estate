60e6009f185c4ae4f7f202e6fc2da3b6bccd1643
Ephraimiyanda <iyandaephraim@gmail.com>
2024-12-02 20:44:11 +0100

    update: authentication callback url fixed

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -17,6 +17,7 @@ export const authOptions = {
       name: "google",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      checks: "none",
     }),
     CredentialsProvider({
       name: "credentials",
@@ -39,12 +40,10 @@ export const authOptions = {
       },
     }),
   ],
-  async redirect({ url, baseUrl }: any) {
-    // Allows relative callback URLs
-    if (url.startsWith("/")) return `${baseUrl}${url}`;
-    // Allows callback URLs on the same origin
-    else if (new URL(url).origin === baseUrl) return url;
-    return baseUrl;
+
+  csrf: {
+    // NextAuth will handle CSRF internally using anti-CSRF tokens
+    csrfToken: true,
   },
 };
 
